ed6514d5a1109ab4d28fdc8ed2da418032be6c0e
Alberto Paladino <alberto.paladino@dualb.it>
2020-06-17 17:02:20 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "ObjectMapper+Realm",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v11),
         .tvOS(.v10),
         .macOS(.v10_10),
     ],
